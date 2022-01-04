b556b4193cab4500b777c3e73d242b70427c70c1
Victor <victorzamorac98@gmail.com>
2022-01-04 13:28:14 -0300

    cambios img

--- src/index.js
+++ src/index.js
@@ -59,19 +59,7 @@ function handleSubmit(event){
   
 }
 
-//boton
-$(document).ready(function(e){
-    $('.btn').on('mouseenter', function(e){
-        x = e.pageX - $(this).offset().left;
-        y = e.pageY - $(this).offset().top;
-        $(this).find('span').css({top:y, left:x})
-    })
-    $('.btn').on('mouseenter', function(e){
-        x = e.pageX - $(this).offset().left;
-        y = e.pageY - $(this).offset().top;
-        $(this).find('span').css({top:y, left:x})
-    })
-})
+
 
 
 
